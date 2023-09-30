e48b82ba2c8491aef54ee88970a8739bca0c393d
SlazZzor <slazzzor@gmail.com>
2023-09-30 15:30:48 +0200

    added git profile

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import blueLogoGif from "./assets/logos/blueSlazzzor.com.gif";
-import Loading from './assets/modules/loading';
-import GithubContainer from './assets/modules/githubContainer';
+import Loading from "./assets/modules/loading";
+const GithubContainer = React.lazy(() => import ("./assets/modules/githubContainer"));
+
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     document.onreadystatechange = () => {
-      if (document.readyState === 'complete') {
-        setIsLoaded(true);
-      }
-    }
+      setTimeout(() => setIsLoaded(true), 1500);
+    };
+    if (document.readyState === "complete"){
+      setTimeout(() => setIsLoaded(true), 1500);
+    };
   }, []);
 
   if (!isLoaded) {
     return (
-      <Loading />
+      <Loading /> 
     )
   };
 
+
   return (
-    <div className="container">
-      <div className="logoContainer">
-        <img src={blueLogoGif} alt="slazzzor.com" />
-      </div> 
-      <div>
-        <GithubContainer />
+      <div className="container">
+        <div>
+          <img src={blueLogoGif} alt="slazzzor.com" />
+        </div> 
+        <div className="profilesContainer">
+          <GithubContainer />
+        </div>
       </div>
-    </div>
   );
 }
 
--- src/assets/modules/githubContainer.js
+++ src/assets/modules/githubContainer.js
@@ -1,26 +1,76 @@
 import '../../App.css';
 import {useEffect, useState} from "react";
 import githubLogo from "../logos/githubLogo.png"
+import blankPfp from "../logos/blankPfp.png"
 
 function GithubContainer() {
-    const [profileInfo, setProfileInfo] = useState({})
+    const [profileInfo, setProfileInfo] = useState()
+    const [reposInfo, setReposInfo] = useState();
+    const [isProfileLoaded, setIsProfileLoaded] = useState(false);
+
+
+    function renderRepos(){
+        if (reposInfo) {
+            return reposInfo.sort((a, b) => (Date.parse(b["pushed_at"]) - Date.parse(a["pushed_at"]))).map((value, index) => 
+                (
+                    <div className="gitRepoContainer" key={index}>
+                        <div className="gitTextContainer">
+                            <a className="gitRepoText" href={value["html_url"]}  target="_blank" rel="noopener noreferrer"> {value["name"]} </a>
+                        </div>
+                        <div className="gitInfoContainer">
+                            <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="100" data-view-component="true">
+                                <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path>
+                            </svg>
+                            <div>{value["stargazers_count"]}</div>
+                            <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="100" data-view-component="true">
+                                <path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"></path>
+                            </svg>
+                            <div>{value["forks_count"]}</div>
+                            <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="100" data-view-component="true">
+                                <path d="M8 2c1.981 0 3.671.992 4.933 2.078 1.27 1.091 2.187 2.345 2.637 3.023a1.62 1.62 0 0 1 0 1.798c-.45.678-1.367 1.932-2.637 3.023C11.67 13.008 9.981 14 8 14c-1.981 0-3.671-.992-4.933-2.078C1.797 10.83.88 9.576.43 8.898a1.62 1.62 0 0 1 0-1.798c.45-.677 1.367-1.931 2.637-3.022C4.33 2.992 6.019 2 8 2ZM1.679 7.932a.12.12 0 0 0 0 .136c.411.622 1.241 1.75 2.366 2.717C5.176 11.758 6.527 12.5 8 12.5c1.473 0 2.825-.742 3.955-1.715 1.124-.967 1.954-2.096 2.366-2.717a.12.12 0 0 0 0-.136c-.412-.621-1.242-1.75-2.366-2.717C10.824 4.242 9.473 3.5 8 3.5c-1.473 0-2.825.742-3.955 1.715-1.124.967-1.954 2.096-2.366 2.717ZM8 10a2 2 0 1 1-.001-3.999A2 2 0 0 1 8 10Z"></path>
+                            </svg>
+                            <div>{value["watchers_count"]}</div>
+                        </div>
+                    </div>
+                )
+            )
+        }
+    };
 
     useEffect(() => {
         fetch(`https://api.github.com/users/slazzzor`)
             .then(result => result.json()
             .then(jsonResult => {
                 setProfileInfo(jsonResult);
-                console.log(jsonResult)
+                setIsProfileLoaded(true);
             }))
     }, []);
 
+    useEffect(() => {
+        fetch(`https://api.github.com/users/SlazZzor/repos`)
+            .then(result => result.json()
+            .then(jsonResult => {
+                setReposInfo(jsonResult);
+                console.log(jsonResult)
+            }))
+    }, []);
+    
+
 
     return (
-        <div className="gitContainer">
-            <img src={githubLogo} alt="git logo" height={75}></img>
-            <img src={profileInfo["avatar_url"]} alt="git pfp" className="gitImg" height={50}></img>
-            <a className="gitText" href="https://github.com/SlazZzor" target="_blank" rel="noopener noreferrer"> SlazZzor </a>
+        <div className="git">
+            <div style={{position:"absolute", opacity: 0.2}}>
+                <img src={githubLogo} alt="git logo" height={50} width={50} loading="lazy"></img>
+            </div>
+            <div className="gitLogosContainer">
+                <img src={isProfileLoaded ? profileInfo["avatar_url"] : blankPfp} alt="git pfp" className="gitImg" height={50} width={50} loading="lazy"></img>
+                <a className="gitText" href="https://github.com/SlazZzor" target="_blank" rel="noopener noreferrer"> SlazZzor </a>
+            </div>
+            <div className="gitReposContainer">
+                {renderRepos()}
+            </div>
         </div>
+        
     );
 }
 
