d5d526ffa6b2fd8bd1665ea35f6973e9bfaed3f3
SlazZzor <slazzzor@gmail.com>
2023-10-02 20:23:33 +0200

    made mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import blueLogoGif from "./assets/logos/blueSlazzzor.com.gif";
+import { useMediaQuery } from 'react-responsive'
 import GithubContainer from "./components/githubContainer";
 import LinkedInContainer from "./components/linkedInContainer";
 import ContactContainer from "./components/contactContainer";
@@ -8,14 +9,18 @@ import ProjectsContainer from "./components/projectsContainer";
 
 
 function App() {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  
   return (
     <div>
       <div className="logo">
           <img src={blueLogoGif} alt="slazzzor.com" />
       </div>
-      <div className="container" id="main">
-          <div className="profilesContainer">
-            <div >
+      <div className="container" id={isDesktopOrLaptop ? "main" : ""}>
+          <div className={isDesktopOrLaptop ? "profilesContainer" : "mobileProfileContainer"}>
+            <div>
               <GithubContainer />
               <LinkedInContainer />
             </div>
